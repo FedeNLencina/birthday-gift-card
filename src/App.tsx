b64e1a5d8fe0c14e2060667dea33feb3009924eb
Federico <federiconahuellencina@gmail.com>
2025-07-19 17:32:10 -0300

    add react boostrap, bitrday card component, formHome component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Route, Routes } from "react-router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FormHome } from "./components/FormHome/FormHome";
+import { BirthdayCard } from "./components/BirthdayCard/BirthdayCard";
+
 function App() {
   return (
-    <>
-      <div>Hola</div>
-    </>
+    <Routes>
+      <Route path="/" element={<FormHome />} />
+      <Route path="/card" element={<BirthdayCard />} />
+    </Routes>
   );
 }
 
---
+++ src/components/FormHome/FormHome.tsx
@@ -0,0 +1,3 @@
+export const FormHome = () => {
+  return <div></div>;
+};
